143feaac57a6c1c2234af60b39c2c462e3ce0d94
funtuk <92526456+PavloPlo@users.noreply.github.com>
2025-12-29 20:59:33 +0100

    Avoid overriding env in Prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import { defineConfig, env } from "prisma/config";
 
 dotenv.config({ path: ".env" });
-dotenv.config({ path: ".env.local", override: true });
+dotenv.config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
